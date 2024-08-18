7812b06fc5bf2c165d1170577d3b6a3f9bec9197
Jayden <jayden.burns@mds.ac.nz>
2024-08-19 11:47:33 +1200

    Cameras Readded
    - Readded Cameras to Main

--- SFMLProject1/main.cpp
+++ SFMLProject1/main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "PlayerCharacter.h"
+#include "cGameCameras.h"
 
 int main()
 {
@@ -11,6 +12,14 @@ int main()
     PlayerCharacter* Player2 = new PlayerCharacter("Resources/Textures/Sprites/Red Player.png", "Player 2");
 
     Player2->SetPosition(sf::Vector2f(500, 400));
+
+    cGameCameras m_Cameras(&window);
+
+    //Temporary Map - Creates texture, loads temp file, changes positioning
+    sf::Texture mapTex;
+    mapTex.loadFromFile("Resources/map.jpg");
+    sf::Sprite map(mapTex);
+    map.setPosition(-sf::Vector2f(map.getLocalBounds().width / 2.f, map.getLocalBounds().height / 2.f));
 
 
     while (window.isOpen())
@@ -55,14 +64,22 @@ int main()
             }
         }
 
+
+        m_Cameras.UpdatePositions(Player1->getPosition(), Player2->getPosition());
+
         window.clear();
+
         //Do all your drawing in here/////
 
         //Player1->Draw();
-        
+        m_Cameras.setView1();
+        window.draw(map);
         window.draw(Player1->playerSprite);
         window.draw(Player2->playerSprite);
-
+        m_Cameras.setView2();
+        window.draw(map);
+        window.draw(Player1->playerSprite);
+        window.draw(Player2->playerSprite);
 
         //////////////////////////////////
         window.display();
