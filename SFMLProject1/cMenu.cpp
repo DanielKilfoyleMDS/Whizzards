01964227ac0f65029f6d2d9c5930e0009766ec5c
Starmanwilll <William.Kuzmic@mds.ac.nz>
2024-10-24 19:26:53 +1300

    Commit Title card
    I did a lot more work than this but it broke on me so i had to restart but forgot the title lol

--- SFMLProject1/cMenu.cpp
+++ SFMLProject1/cMenu.cpp
@@ -6,27 +6,34 @@ cMenu::cMenu(float width, float height) {
         std::cerr << "Failed to load font!" << std::endl;
     }
 
+    // Setup title
+    title.setFont(font);
+    title.setFillColor(sf::Color::Yellow);  // Set color
+    title.setString("WHIZZARDS");           // Set the title text
+    title.setCharacterSize(100);            // Set a larger size for the title
+    title.setPosition(sf::Vector2f(width / 2 - 300, height / 6));  // Adjust position
+
     // Setup "Play" button
     menu[0].setFont(font);
     menu[0].setFillColor(sf::Color::Red);
     menu[0].setString("Play");
-    menu[0].setCharacterSize(50);  // Increase text size for visibility
-    menu[0].setPosition(sf::Vector2f(200, 150));  // Adjust these values
+    menu[0].setCharacterSize(50);
+    menu[0].setPosition(sf::Vector2f(width / 2 - 100, height / 2));
 
     // Setup "Quit" button
     menu[1].setFont(font);
     menu[1].setFillColor(sf::Color::White);
     menu[1].setString("Quit");
-    menu[1].setCharacterSize(50);  // Increase text size for visibility
-    menu[1].setPosition(sf::Vector2f(200, 300));  // Adjust these values
-
+    menu[1].setCharacterSize(50);
+    menu[1].setPosition(sf::Vector2f(width / 2 - 100, height / 2 + 100));
 
     selectedItemIndex = 0;
 }
 
 void cMenu::draw(sf::RenderWindow& window) {
+    window.draw(title);  // Draw the title first
     for (int i = 0; i < MAX_NUMBER_OF_ITEMS; i++) {
-        window.draw(menu[i]);  // Draw menu items
+        window.draw(menu[i]);  // Draw the menu items
     }
 }
 
--- SFMLProject1/cMenu.h
+++ SFMLProject1/cMenu.h
@@ -1,20 +1,19 @@
+
 #include <SFML/Graphics.hpp>
 
 class cMenu {
 public:
     cMenu(float width, float height);
-
     void draw(sf::RenderWindow& window);
-
     void MoveUp();
     void MoveDown();
-
     int GetPressedItem();
 
 private:
     int selectedItemIndex;
     sf::Font font;
-    sf::Text menu[2];
+    sf::Text menu[2];  // Play and Quit buttons
+    sf::Text title;    // New title text
 
     static const int MAX_NUMBER_OF_ITEMS = 2;
 };
